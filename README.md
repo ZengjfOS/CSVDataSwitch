5dbd8b973dfd76393bc6990135fddf9bf7f7a5b8
ZoroZeng <aplexos.@163.com>
2017-05-18 09:34:21 +0800

    add configure file.

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # CSV Data Switch
 
-## 使用说明
-
-* 编码语言采用[Python 3](https://www.python.org/)，请安装Python解释器：[Download Python 3](https://www.python.org/downloads/)
-* 将所有的[csv](http://baike.baidu.com/item/CSV/10739)文件，放入[input](input)目录；  
+## 目录架构
 
 ```
     .
@@ -14,10 +11,18 @@
     │   └── 2_FFFF03362B99_MTCore_Log.csv
     ├── main.py                    <--------- 使用Python3运行该程序
     ├── output.csv                 <--------- main.py程序输出文件
+    ├── config.ini                 <--------- 参考配置文件
     └── README.md
 ```
 
-* 输出文件名：[output.csv](output.csv)
+
+## 使用说明
+
+* 编码语言采用[Python 3](https://www.python.org/)，请安装Python解释器：[Download Python 3](https://www.python.org/downloads/)
+* 将所有的[csv](http://baike.baidu.com/item/CSV/10739)文件，放入[input](input)目录；  
+* 修改[config.ini](config.ini)文件内容以满足对获取列的内容，配置文件标准请参考：[Configuration file parser](https://docs.python.org/3/library/configparser.html#supported-ini-file-structure)
+  * 只需要写**section**部分，其他部分不需要，请参考当前的[config.ini](config.ini)进行修改；
+* 输出文件名：[output.csv](output.csv)；
 
 ## 输出信息
 
--- main.py
+++ main.py
@@ -5,64 +5,108 @@ Description：
     1. 所有的csv文件都在input目录下；
     2. 从csv文件读去Serial Number、Dark Bad Pixels、Dark Long Bad Pixels字段内容；
     3. 将读取去的相关内容写入ouput.csv文件中；
-    4. output.csv文件中，没一行代表对应Serial Number、Dark Bad Pixels、Dark Long Bad Pixels对应的值。
+    4. output.csv文件中，每一行代表对应Serial Number、Dark Bad Pixels、Dark Long Bad Pixels对应的值。
+    5. Serial Number、Dark Bad Pixels、Dark Long Bad Pixels字段放在config.ini文件内部，output输出行的顺序也是依照这个顺序
 
 Author：曾剑锋
 Date：2017-05-17
 '''
 
 import sys
-import getopt
 import csv
+import configparser
 from os import walk
 
+'''
+获取配置文件中的配置
+'''
+def getConfigureHeader(configureFile) :
 
-def getCSVFiles():
+    config = configparser.ConfigParser()
+    config.sections()
+    config.read(configureFile)
+
+    return config.sections()
+
+'''
+获取所有需要处理csv文件
+'''
+def getCSVFiles() :
 
     f = []
-    for (dirpath, dirnames, filenames) in walk("input"):
+    for (dirpath, dirnames, filenames) in walk("input") :
         f.extend(filenames)
 
     print(f)
 
     return f
 
-def dealCSVFile(inputFile, output):
+'''
+检查当前行里是否有需要的列值
+'''
+def getValueFromRow(row, header, columnWithVals) :
+
+    for column in header :
+        if row[0].strip() == column.strip() :
+            if len(row[1].strip()) != 0 :
+                columnWithVals[column.strip()] = row[1].strip();
+                return row[1].strip()
+
+    return None
+
+'''
+获取当前文件中所有的row，对每行数据进行对比，见最后输出的结果输出到output.csv文件中
+'''
+def dealCSVFile(inputFile, output, outputHeader) :
 
     outputRow = []
+    columnWithVals = {}
 
-    # data row number was: 11
-    for i in range(10):
-        row = next(inputFile)
+    # remove header before deal data
+    inputHeader = next(inputFile)
 
-    # get serial number
-    row = next(inputFile)
-    outputRow.append(row[1])
+    for row in inputFile :
+        if (len(row) >= 2) :
 
-    # data row number was: 39
-    for i in range(38 - 11):
-        row = next(inputFile)
+            # get values in dict
+            getValueFromRow(row, outputHeader, columnWithVals)
 
-    row = next(inputFile)
-    outputRow.append(row[1])
+    # order all vaule by outputHeader order
+    for column in outputHeader :
+        outputRow.append(columnWithVals[column])
 
-    row = next(inputFile)
-    outputRow.append(row[1])
-
+    # write to output file
     output.writerow(outputRow)
 
+'''
+主线程处理函数
+'''
 def main(argv=None):
 
+    # get all csv file
     csvFiles = getCSVFiles()
 
+    # create output.csv file and get a csv writer
     csv_output = open('output.csv','w')
     outputCSVWriter = csv.writer(csv_output, delimiter=',', lineterminator='\n')
-    outputCSVWriter.writerow(["Serial Number", "Dark Bad Pixels", "Dark Long Bad Pixels"])
+
+    # get output csv header from config.ini file
+    outputHeader = getConfigureHeader("config.ini")
+    if (outputHeader == None) :
+        return
+    outputCSVWriter.writerow(outputHeader)
 
     for inputFile in csvFiles:
         csv_input = open("input/" + inputFile)
         inputCSVReader = csv.reader(csv_input)
-        dealCSVFile(inputCSVReader, outputCSVWriter)
+
+        dealCSVFile(inputCSVReader, outputCSVWriter, outputHeader)
+
+        csv_input.flush()
+        csv_input.close()
+
+    csv_output.flush()
+    csv_output.close()
 
 if __name__ == "__main__":
     sys.exit(main())
